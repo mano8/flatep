e3c6c6e273804c080a3e02bc01c87575ff26a65f
mano8 <mex.serra@gmail.com>
2020-03-20 11:20:37 +0100

    empty Flatsome child Base

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * FlaTep Functions.php
+ *
+ * @package FlaTep
+ */
+
+
+ /**
+ * add child styles
+ *
+ * @since   1.0.0
+ */
+
+function theme_enqueue_flatep_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_flatep_styles', 10 );
