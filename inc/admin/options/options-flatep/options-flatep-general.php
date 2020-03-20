9bb2cd7933245fb0fe9ea6ee16c1c4f550cfa57a
mano8 <mex.serra@gmail.com>
2020-03-20 15:19:21 +0100

    Add control to debug mode and on display of html, scripts and styles import version.

--- inc/admin/options/options-flatep/options-flatep-general.php
+++ inc/admin/options/options-flatep/options-flatep-general.php
@@ -2,10 +2,10 @@
 
 Flatsome_Option::add_field( '', array(
 	'type'     => 'custom',
-	'settings' => 'general_title_general_child',
+	'settings' => 'flatep_title_general_child',
 	'label'    => __( '', 'flatsome-admin' ),
 	'section'  => 'flatep-general-options',
-	'default'  => '<div class="options-title-divider">General :</div>',
+	'default'  => '<div class="options-title-divider">General Options :</div>',
 ) );
 
 Flatsome_Option::add_field( 'option', array(
@@ -18,8 +18,35 @@ Flatsome_Option::add_field( 'option', array(
 
 Flatsome_Option::add_field( 'option', array(
 	'type'     => 'checkbox',
+	'settings' => 'flatep_debug',
+    'label'    => __( 'Active debug.', 'flatsome-admin' ),
+    'description' => __( 'Show debug on Query monitor plugin and html source.', 'flatsome-admin' ),
+	'section'  => 'flatep-general-options',
+	'default'  => false,
+) );
+
+
+Flatsome_Option::add_field( '', array(
+	'type'     => 'custom',
+	'settings' => 'flatep_title_display_versions',
+	'label'    => __( '', 'flatsome-admin' ),
+	'section'  => 'flatep-general-options',
+	'default'  => '<div class="options-title-divider">Versions Display :</div>',
+) );
+
+Flatsome_Option::add_field( 'option', array(
+	'type'     => 'checkbox',
 	'settings' => 'tep_disable_generator_version',
 	'label'    => __( 'Remove meta generator version', 'flatsome-admin' ),
+	'section'  => 'flatep-general-options',
+	'default'  => false,
+) );
+
+Flatsome_Option::add_field( 'option', array(
+	'type'     => 'checkbox',
+	'settings' => 'tep_disable_head_version',
+	'label'    => __( 'Remove All js and css import versions.', 'flatsome-admin' ),
+	'description' => __( 'Active when finish web creation. Enable Navigator caching.', 'flatsome-admin' ),
 	'section'  => 'flatep-general-options',
 	'default'  => false,
 ) );
--- inc/structure/structure-header.php
+++ inc/structure/structure-header.php
@@ -12,10 +12,10 @@ class FlaTep_Header{
 
     private $debug;
 
-    function __construct($debug=false){
+    function __construct(){
 		// Start the 'foo' timer:
-		$this->debug = ($debug === true) ? true : false;
-		
+		$this->debug = (get_theme_mod( 'flatep_debug', false ) === true) ? true : false;
+        
 		if($this->debug) {do_action( 'qm/debug', 'Start FlaTep Header Structure Class' );}
 		if($this->debug) {do_action( 'qm/start', 'FlaTep_Header' );}
 		$this->run();
@@ -38,16 +38,20 @@ class FlaTep_Header{
 	 * @since   1.0.0
 	 */
 	public function run(){
-        $this->init_data();                                                     //-> init data parameters
-		add_filter('the_generator', array($this, 'remove_wordpress_version'));  // remove versions from meta
-		
+        $this->init_data(); //-> init data parameters
+        // remove wordpress version from meta
+		add_filter('the_generator', array($this, 'remove_wordpress_version'));
+        
+        //-> update styles and scripts loader tags
+        add_filter( 'style_loader_tag', array($this, 'loader_tags_update'), 99, 3 );
+		add_filter( 'script_loader_tag', array($this, 'loader_tags_update'), 99, 3 );
     }
     
 
     /**
 	 * Remove meta generator version
      * 
-     * Meta generator display wordpress version in header
+     * Meta generator display wordpress version in header tep_disable_head_version
 	 *
 	 * @since   1.0.0
 	 */
@@ -55,6 +59,25 @@ class FlaTep_Header{
 		if( get_theme_mod( 'tep_disable_generator_version', true ) ){
 			return '';
 		}
+    }
+    
+    /**
+	 * Add cdn integrity parameters to imported script and styles
+	 *
+	 * @since   1.0.0
+	 */
+	public function loader_tags_update($html, $handle, $src){
+		//-> remove versions from styles and scripts
+		if(get_theme_mod( 'tep_disable_head_version', false )){
+			if ( strpos( $src, 'ver=' ) ){
+				// 
+				$src_o = remove_query_arg( 'ver', $src );
+				$html = str_replace($src, $src_o, $html);
+				$src = $src_o;
+			}
+		}
+		$debug = ($this->debug) ? ' <!--Handle : '.sanitize_key($handle).'-->' : '';
+		return ($html . $debug);
 	}
 }
 
