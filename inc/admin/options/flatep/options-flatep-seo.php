e04607ae5cde9fd0125ec0c44196bbfd9710c80c
mano8 <mex.serra@gmail.com>
2020-03-23 20:25:17 +0100

    - Modify shortcodes share_fallow and product_categories  - Add Woocommerce link accessibility  - Add woocommerce custom meta key years option

--- inc/admin/options/flatep/options-flatep-seo.php
+++ inc/admin/options/flatep/options-flatep-seo.php
@@ -19,3 +19,47 @@ Flatsome_Option::add_field( 'option', array(
 	'section'       => 'flatep-options-seo',
 	'default'       => false,
 ) );
+
+Flatsome_Option::add_field( '', array(
+	'type'     => 'custom',
+	'settings' => 'flatep_title_seo_woo',
+	'label'    => __( '', 'flatsome-admin' ),
+	'section'  => 'flatep-options-seo',
+	'default'  => '<div class="options-title-divider">Woocommerce :</div>',
+) );
+
+Flatsome_Option::add_field( 'option', array(
+	'type'        => 'text',
+	'settings'    => 'flatep_seo_woo_cat_link_before',
+	'label'       => __( 'Words to add before category title links', 'flatsome-admin' ),
+	'description' => __( 'Category links title semm : <br /> Before Category_name After', 'flatsome-admin' ),
+	'section'     => 'flatep-options-seo',
+	'transport'   => 'postMessage',
+) );
+
+Flatsome_Option::add_field( 'option', array(
+	'type'        => 'text',
+	'settings'    => 'flatep_seo_woo_cat_link_after',
+	'label'       => __( 'Words to add after category title links', 'flatsome-admin' ),
+	'description' => __( 'Category links title semm : <br /> Before Category_name After', 'flatsome-admin' ),
+	'section'     => 'flatep-options-seo',
+	'transport'   => 'postMessage',
+) );
+
+Flatsome_Option::add_field( 'option', array(
+	'type'        => 'text',
+	'settings'    => 'flatep_seo_woo_prod_link_before',
+	'label'       => __( 'Words to add before products title links', 'flatsome-admin' ),
+	'description' => __( 'Products links title semm : <br /> Before Product_name After', 'flatsome-admin' ),
+	'section'     => 'flatep-options-seo',
+	'transport'   => 'postMessage',
+) );
+
+Flatsome_Option::add_field( 'option', array(
+	'type'        => 'text',
+	'settings'    => 'flatep_seo_woo_prod_link_after',
+	'label'       => __( 'Words to add after products title links', 'flatsome-admin' ),
+	'description' => __( 'Products links title semm : <br /> Before Product_name After', 'flatsome-admin' ),
+	'section'     => 'flatep-options-seo',
+	'transport'   => 'postMessage',
+) );
