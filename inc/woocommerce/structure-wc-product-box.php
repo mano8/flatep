54e8095eb4313abcda4afe48b505b401084ca85e
mano8 <mex.serra@gmail.com>
2020-08-03 03:23:31 +0200

    update to flatsome 3.12.0

--- inc/woocommerce/structure-wc-product-box.php
+++ inc/woocommerce/structure-wc-product-box.php
@@ -87,7 +87,7 @@ if ( ! function_exists( 'woocommerce_template_loop_product_title' ) ) {
         $title = get_the_title();
         $link_attr = (class_exists("FlaTep_Woocommerce")) ? $link_attr = FlaTep_Woocommerce::get_the_product_link_title_seo($title) : $title;
         
-		echo '<p class="name product-title"><a href="' . get_the_permalink() . '" '. $link_attr . '>' . $title . '</a></p>';
+		echo '<p class="name product-title ' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '"><a href="' . get_the_permalink() . '" '. $link_attr . '>' . $title . '</a></p>';
 	}
 }
 
